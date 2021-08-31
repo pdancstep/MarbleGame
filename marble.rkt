97c327f2d2746028f6b221d40615f46c1f9af58d
Jamie Perconti <jamieperconti@gmail.com>
2021-08-31 13:12:02 -0400

    generalize level construction & start figuring out event handling

--- marble.rkt
+++ marble.rkt
@@ -1,35 +1,46 @@
 #lang racket
 (require plot)
-
 [plot-x-ticks no-ticks]
 [plot-y-ticks no-ticks]
 [plot-x-label #f]
 [plot-y-label #f]
 
 (define unit-circle (polar (λ (θ) 1) #:color 'black))
+(define PLOT-X-MIN -4)
+(define PLOT-X-MAX 4)
+(define PLOT-Y-MIN -4)
+(define PLOT-Y-MAX 4)
 
 (define (marble x y #:color [c 'black])
-  (points `((,x ,y)) #:sym 'fullcircle #:size 12 #:color c))
+  (cons `(marble ,x ,y)
+        (points `((,x ,y)) #:sym 'fullcircle #:size 12 #:color c)))
 
-(define (htrack xmin xmax y #:color [c 'lightblue])
-  (lines `((,xmin ,y) (,xmax ,y)) #:width 18 #:color c))
+(define (htrack xmin xmax y [type '+] #:color [c 'lightblue])
+  (cons `(track linear ((,xmin ,y) (,xmax ,y)) ,type)
+        (lines `((,xmin ,y) (,xmax ,y)) #:width 18 #:color c)))
 
-(define (rot-track θmin θmax r #:color [c 'orange])
-  (polar (λ (θ) r) θmin θmax #:width 18 #:color c))
+(define (rot-track θmin θmax r [type '+] #:color [c 'orange])
+  (cons `(track arc TODO ,type)
+        (polar (λ (θ) r) θmin θmax #:width 18 #:color c)))
 
-(define level
-  (plot (list unit-circle
-              (htrack -2 2 -2)
-              (rot-track (/ pi 2) pi 3)
-              (marble 2 -2)
-              (marble -3 0))
-        #:x-min -4
-        #:x-max 4
-        #:y-min -4
-        #:y-max 4))
+(define ((build-mouse-handler components) level event x y)
+  (cond [(send event button-down? 'left)
+         ; on mouse-down, determine which marble and track we are on
+         ]
+        [(send event button-up? 'left) #f]
+        [(send event dragging?) #f]))
 
-; figure out what a callback function for mouse events looks like, write it,
-; and put it in place of the #f here
-(send level set-mouse-event-callback #f)
+(define (make-level components)
+  (define level-plot
+    (plot (cons unit-circle (map cdr components))
+          #:x-min PLOT-X-MIN
+          #:x-max PLOT-X-MAX
+          #:y-min PLOT-Y-MIN
+          #:y-max PLOT-Y-MAX))
+  (send level-plot set-mouse-event-callback (build-mouse-handler components))
+  level-plot)
 
-level
+(make-level (list (htrack -2 2 -2)
+                  (rot-track (/ pi 2) pi 3 '*)
+                  (marble 2 -2)
+                  (marble -3 0)))
