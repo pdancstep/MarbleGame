4666b2c124f8cc41f2055bf2a17ad7fedd133ea5
pdancstep <77130138+pdancstep@users.noreply.github.com>
2021-09-22 19:05:28 -0700

    Added "trapped follower" maze level

--- game.rkt
+++ game.rkt
@@ -63,3 +63,25 @@
                     (make-driver 0 2/3)
                     (make-htrack -1/3 1/3 -1 '+)
                     (make-driver -1/3 -1))))
+
+(define example6
+  (make-level (list (for/list ([n '(-2 -1 0 1 2)])
+                      (list (make-htrack -2 2 (- n 1) '+) (make-vtrack n -3 1 '+)))
+
+                    (make-htrack -2 -2.5 -3 '+)
+                    (make-goal -2.5 -3 #:color 'lightblue)
+
+                    ;block
+                    (make-htrack -.5 .5 3.5)
+                    (make-htrack -.5 .5 3.15)
+                    (make-htrack -.5 .5 2.85)
+                    (make-htrack -.5 .5 2.5)
+                    (make-vtrack -.5 2.5 3.5)
+                    (make-vtrack .5 2.5 3.5)
+
+                    (make-driver .5 -1)
+                    (make-driver -.5 -1)
+                    (make-follower 0 2.5))))
+
+example6
+
