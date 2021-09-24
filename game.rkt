2288bb928511ef824c8ed3797d0cc6652bdb36f2
pdancstep <77130138+pdancstep@users.noreply.github.com>
2021-09-24 09:25:01 -0700

    Cleaned up some examples - made a maze.

--- game.rkt
+++ game.rkt
@@ -1,6 +1,7 @@
 #lang racket
 (require "components.rkt" "level.rkt")
 
+;circuitous path
 (define example1
   (make-level (list (make-htrack -2.5 2.5 0)
                     (make-rot-track 0 (* 2 pi) .5)
@@ -15,17 +16,19 @@
                     (make-driver -3.5 0)
                     (make-follower -2.5 0))))
 
+;Across the origin (solvable in three moves)
 (define example2
-  (make-level (list (make-vtrack 0 -1 1.5)
-                    (make-goal 0 -1)
-                    (make-follower 0 1.5)
+  (make-level (list (make-vtrack 0 1.2 -.6)
+                    (make-goal 0 -.6)
+                    (make-follower 0 1.2)
 
-                    (make-vtrack 0 2 3.5 '*)
-                    (make-driver 0 3.5)
+                    (make-vtrack 0 3.6 1.8 '*)
+                    (make-driver 0 3.6)
 
-                    (make-vtrack 0 -3 -2 '+)
-                    (make-driver 0 -2))))
+                    (make-vtrack 0 -1.85 -2.75 '+)
+                    (make-driver 0 -1.85))))
 
+;fixed point puzzle
 (define example3
   (make-level (list (make-htrack -2.5 2.5 0)
                     (make-rot-track pi (* 1.25 pi) 2.5)
@@ -40,6 +43,7 @@
                     (make-driver (* 2.5 (cos (* .625 pi))) (* 2.5 (sin (* .625 pi))))
                     (make-follower (* 2.5 (cos (* 1.25 pi))) (* 2.5 (sin (* 1.25 pi)))))))
 
+;cartesian maze
 (define example4
   (make-level (list (make-vtrack -3 -2 2.5 '+)
                     (make-driver -3 -2)
@@ -47,12 +51,32 @@
                     (make-driver -2 -3)
                     
                     (make-follower -2 -2)
-                    ; TODO make grid into an actual maze
-                    (for/list ([n '(-2 -1 0 1 2)])
-                      (list (make-htrack -2 2 n) (make-vtrack n -2 2)))
+
+                    (make-htrack -2 1.5 2)
+                    (make-htrack -2 1.5 1)
+                    (make-htrack -2 -1.5 0)
+                    (make-htrack -1 1.5 0)
+                    (make-htrack -1.5 0 -1)
+                    (make-htrack .5 2 -1)
+                    (make-htrack -2 -1.5 -2)
+                    (make-htrack -1 2 -2)
+
+                    (make-vtrack -2 2 -2)
+                    (make-vtrack -1 2 .5)
+                    (make-vtrack -1 0 -.5)
+                    (make-vtrack -1 -1 -2)
+                    (make-vtrack 0 2 1)
+                    (make-vtrack 0 .5 -1)
+                    (make-vtrack 0 -1.5 -2)
+                    (make-vtrack 1 2 1.5)
+                    (make-vtrack 1 1 0)
+                    (make-vtrack 1 -.5 -2)
+                    (make-vtrack 2 2.5 -2)
+
                     (make-goal 2 2.5)
                     (make-vtrack 2 2 2.5))))
 
+;inchworm (solvable in 10 moves)
 (define example5
   (make-level (list (make-htrack 1/3 10/3 0)
                     (make-follower 1/3 0)
@@ -63,7 +87,7 @@
                     (make-htrack -1/3 1/3 -1 '+)
                     (make-driver -1/3 -1))))
 
-
+;confined follower maze
 (define example6
   (make-level (list (for/list ([n '(-2 -1 0 1 2)])
                       (list (make-htrack -2 2 (- n 1) '+) (make-vtrack n -3 1 '+)))
@@ -83,6 +107,7 @@
                     (make-driver -.5 -1)
                     (make-follower 0 2.5))))
 
+;similar paths, different scales
 (define example7
   (make-level (list (make-htrack -3 -1 .5)
                     (make-vtrack -1 .5 2.5)
@@ -107,6 +132,7 @@
                     (make-driver .5 -2.5)
                     (make-follower -3 .5))))
 
+;smiley face?
 (define example8
   (make-level (list (make-vtrack 0 2 4)
                     (make-rot-track (* 1/4 pi) (* 1/2 pi) 3)
