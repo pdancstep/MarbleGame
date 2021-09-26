3516bd39cfc7be9f6ca02ce6fb3bee073bd65f8d
pdancstep <77130138+pdancstep@users.noreply.github.com>
2021-09-25 17:23:16 -0700

    More new levels

--- game.rkt
+++ game.rkt
@@ -8,6 +8,7 @@
                     (make-rot-track 0 (* 2 pi) 1.5)
                     (make-goal 2.5 0)
 
+                    (make-rot-track -.001 0 3.5 '*)
                     (make-rot-track 0 pi 3.5 '*)
                     (make-htrack -.5 .5 -2.5 '+)
 
@@ -107,7 +108,7 @@
                     (make-driver -.5 -1)
                     (make-follower 0 2.5))))
 
-;similar paths, different scales
+;similar paths, half scale
 (define example7
   (make-level (list (make-htrack -3 -1 .5)
                     (make-vtrack -1 .5 2.5)
@@ -132,8 +133,51 @@
                     (make-driver .5 -2.5)
                     (make-follower -3 .5))))
 
-;smiley face?
+
+;similar paths, quarter scale
 (define example8
+  (make-level (list (make-htrack -3 -1 .5)
+                    (make-vtrack -1 .5 2.5)
+                    (make-htrack -1 1 2.5)
+                    (make-vtrack 1 2.5 .5)
+                    (make-htrack 1 3 .5)
+                    (make-goal 3 .5)
+
+                    (make-htrack -3.75 -3.25 -2.5 '+)
+                    (make-vtrack -3.25 -2.5 -2 '+)
+                    (make-htrack -3.25 -2.75 -2 '+)
+                    (make-vtrack -2.75 -2 -2.5 '+)
+                    (make-htrack -2.75 -2.25 -2.5 '+)
+
+                    (make-htrack -1.75 -1.25 -2.5 '+)
+                    (make-vtrack -1.25 -2.5 -2 '+)
+                    (make-htrack -1.25 -.75 -2 '+)
+                    (make-vtrack -.75 -2 -2.5 '+)
+                    (make-htrack -.75 -.25 -2.5 '+)
+
+                    (make-htrack 1.75 1.25 -2.5 '+)
+                    (make-vtrack 1.25 -2.5 -2 '+)
+                    (make-htrack 1.25 .75 -2 '+)
+                    (make-vtrack .75 -2 -2.5 '+)
+                    (make-htrack .75 .25 -2.5 '+)
+
+                    (make-htrack 3.75 3.25 -2.5 '+)
+                    (make-vtrack 3.25 -2.5 -2 '+)
+                    (make-htrack 3.25 2.75 -2 '+)
+                    (make-vtrack 2.75 -2 -2.5 '+)
+                    (make-htrack 2.75 2.25 -2.5 '+)
+
+
+
+                    (make-driver -3.75 -2.5)
+                    (make-driver -1.75 -2.5)
+                    (make-driver .25 -2.5)
+                    (make-driver 2.25 -2.5)
+                    (make-follower -3 .5))))
+
+
+;smiley face?
+(define example9
   (make-level (list (make-vtrack 0 2 4)
                     (make-rot-track (* 1/4 pi) (* 1/2 pi) 3)
                     (make-htrack (* 3 (cos (* 1/4 pi))) (+ 1 (* 3 (cos (* 1/4 pi)))) (* 3 (sin (* 1/4 pi))))
@@ -152,8 +196,8 @@
                     (make-driver 1 0)
                     (make-follower 0 3))))
 
-;two goals at once
-(define example9
+;two slugs
+(define example10
   (make-level (list (make-vtrack 0 -1 -3 '*)
                     
                     (make-htrack -1 0 2 '+)
@@ -172,10 +216,109 @@
                     (make-goal -3 1)
 
                     (make-driver 0 -3)
-                    (make-driver 0 2)
+                    (make-driver 0 3)
 
-                    (make-follower -3 0 )
-                    (make-follower 3 0 ))))
+                    (make-follower -3 1 )
+                    (make-follower 3 1 ))))
+
+
+;two point turnaround
+(define example11
+  (make-level (list (make-rot-track 0 (* 1/2 pi) 2)
+                    (make-rot-track pi (* 3/2 pi) 2)
+                    (make-htrack -2 3 0)
+                    (make-vtrack 0 -2 2)
+
+                    (make-goal 1 0)
+                    (make-goal 3 0)
+
+                    (make-htrack -3 -2 1 '+)
+                    (make-vtrack -3 1 3 '+)
+                    (make-htrack -3 -2 3 '+)
+
+                    (make-rot-track (* 5/4 pi) (* 7/4 pi) 3 '*)
+
+                    (make-follower -1 0)
+                    (make-follower 1 0)
+
+                    (make-driver (* 3 (cos (* 5/4 pi))) (* 3 (sin (* 5/4 pi))))
+                    (make-driver -2 1))))
+
+;circumferential paths (kinda messy)...
+(define example12
+  (make-level (list ;center diamond
+                    (make-linear-track 0 1 -1 0 '+)
+                    (make-linear-track -1 0 0 -1 '+)
+                    (make-linear-track 0 -1 1 0 '+)
+                    (make-linear-track 1 0 0 1 '+)
+
+                    ;inner circle
+                    (make-rot-track 0 (* 2 pi) 1.5 '*)
+                    (make-linear-track -.45 2.5 .55 3.5)
+
+                    ;quadrant 1
+                    (make-rot-track (* .05 pi) (* .45 pi) 3.5)
+                    (make-rot-track (* .08 pi) (* .42 pi) 2.5)
+
+                    ;quadrant 2
+                    (make-rot-track (* 1/2 pi) (* .95 pi) 3.5)
+                    (make-rot-track (* .55 pi) (* .92 pi) 2.5)
+
+                    (make-linear-track -3.5 .5 -2.5 -.5)
+                    (make-linear-track -3.5 -.5 -2.5 .5)
+
+                    ;quadrant 3/4
+                    (make-rot-track (* 1.05 pi) (* 1.95 pi) 3.5)
+                    (make-rot-track (* 1.08 pi) (* 1.92 pi) 2.5)
+
+                    (make-linear-track -.5 -2.5 .5 -3.5)
+                    (make-linear-track -.5 -3.5 .5 -2.5)
+
+                    (make-linear-track 3.5 .5 2.5 -.5)
+                    (make-linear-track 3.5 -.5 2.5 .5)
+
+
+
+                    (make-goal (* 2.5 (cos (* .42 pi))) (* 2.5 (sin (* .42 pi))))
+                    
+                    (make-driver 0 1)
+                    (make-driver 0 1.5)
+                    (make-follower 0 3.5))))
+
+
+;helmut maze
+(define example13
+  (make-level (list ;orange tracks
+                    (make-rot-track (* 3/4 pi) (* 5/4 pi) 3.5 '*)
+                    (make-rot-track (* 7/4 pi) (* 9/4 pi) 3.5 '*)
+
+                    (make-linear-track (* 4.4 (cos (* 21/16 pi))) (* 4.4 (sin (* 21/16 pi)))
+                                       (* 2.2 (cos (* 21/16 pi))) (* 2.2 (sin (* 21/16 pi))) '*)
+                    (make-linear-track (* 4.4 (cos (* 27/16 pi))) (* 4.4 (sin (* 27/16 pi)))
+                                       (* 2.2 (cos (* 27/16 pi))) (* 2.2 (sin (* 27/16 pi))) '*)
+
+                    ;grey tracks
+                    (make-htrack -.75 .75 0)
+                    (make-vtrack 0 -1.5 1.5)
+                    (make-rot-track 0 (* 2 pi) .75)
+                    (make-rot-track pi (* 2.88 pi) 1.5)
+
+                    (make-htrack -3 -1.5 0)
+                    (make-htrack 3 1.5 0)
+                    (make-rot-track 0 pi 3)
+
+                    (make-goal -1.5 0)
+                    
+                    (make-driver (* 2.2 (cos (* 21/16 pi))) (* 2.2 (sin (* 21/16 pi))))
+                    (make-driver (* 2.2 (cos (* 27/16 pi))) (* 2.2 (sin (* 27/16 pi))))
+
+                    (make-driver (* 3.5 (cos (* 5/4 pi))) (* 3.5 (sin (* 5/4 pi))))
+                    (make-driver (* 3.5 (cos (* 9/4 pi))) (* 3.5 (sin (* 9/4 pi))))
+
+                    (make-follower -.75 0 ))))
+
+                    
+                                       
 
 
 
