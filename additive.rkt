88dad7b11967990897b0c7335ceb1a83a0886a3b
Jamie Perconti <jamieperconti@gmail.com>
2021-10-01 14:41:50 -0400

    redesign linear2 (now renamed similar2) to something more interesting, if still tedious

--- additive.rkt
+++ additive.rkt
@@ -41,7 +41,7 @@
                     (make-driver -2 0))))
 
 ;similar paths, half scale
-(define linear1
+(define similar1
   (make-level (list (make-htrack -3 -1 .5)
                     (make-vtrack -1 .5 2.5)
                     (make-htrack -1 1 2.5)
@@ -66,36 +66,49 @@
                     (make-follower -3 .5))))
 
 ;similar paths, third scale
-(define linear2
-  (make-level (list (make-htrack -3 -1 .5)
-                    (make-vtrack -1 .5 2)
-                    (make-htrack -1 1 2)
-                    (make-vtrack 1 2 .5)
-                    (make-htrack 1 3 .5)
-                    (make-goal 3 .5)
+(define similar2
+  (make-level (make-htrack -3 -1 1/2)
+              (make-vtrack -1 0 2)
+              (make-htrack -1 1 2)
+              (make-vtrack 1 2 0)
+              (make-htrack 1 3 1/2)
+              (make-goal 3 .5)
+              (make-follower -3 1/2 'big)
+              (make-htrack -1 1 0)
+              (make-vtrack 0 -1 0)
+              (make-goal 0 -1)
+              (make-follower 0 -1 'small)
+              
+              (make-htrack -3 -7/3 -5/2 '+)
+              (make-vtrack -7/3 -5/2 -2 '+)
+              (make-htrack -7/3 -5/3 -2 '+)
+              (make-vtrack -5/3 -2 -5/2 '+)
+              (make-htrack -5/3 -1 -5/2 '+)
+              (make-driver -3 -5/2 'big)
+              
+              (make-htrack -1 -1/3 -2 '+)
+              (make-vtrack -1/3 -2 -5/2 '+)
+              (make-htrack -1/3 1/3 -5/2 '+)
+              (make-vtrack 1/3 -5/2 -2 '+)
+              (make-htrack 1/3 1 -2 '+)
+              (make-driver -1 -2 'big)
+              
+              (make-htrack 1 5/3 -5/2 '+)
+              (make-vtrack 5/3 -5/2 -2 '+)
+              (make-htrack 5/3 7/3 -2 '+)
+              (make-vtrack 7/3 -2 -5/2 '+)
+              (make-htrack 7/3 3 -5/2 '+)
+              (make-driver 1 -5/2 'big)
 
-                    (make-htrack -3 -7/3 -2 '+)
-                    (make-vtrack -7/3 -2 -3/2 '+)
-                    (make-htrack -7/3 -5/3 -3/2 '+)
-                    (make-vtrack -5/3 -3/2 -2 '+)
-                    (make-htrack -5/3 -1 -2 '+)
+              (make-vtrack -8/3 3/2 2 '+)
+              (make-htrack -8/3 -2 2 '+)
+              (make-vtrack -2 3/2 2 '+)
+              (make-driver -2 3/2 '(big small) #:color 'red)
 
-                    (make-htrack -1 -1/3 -5/2 '+)
-                    (make-vtrack -1/3 -5/2 -2 '+)
-                    (make-htrack -1/3 1/3 -2 '+)
-                    (make-vtrack 1/3 -2 -5/2 '+)
-                    (make-htrack 1/3 1 -5/2 '+)
-
-                    (make-htrack 1 5/3 -2 '+)
-                    (make-vtrack 5/3 -2 -3/2 '+)
-                    (make-htrack 5/3 7/3 -3/2 '+)
-                    (make-vtrack 7/3 -3/2 -2 '+)
-                    (make-htrack 7/3 3 -2 '+)
-
-                    (make-driver -3 -2)
-                    (make-driver -1 -5/2)
-                    (make-driver 1 -2)
-                    (make-follower -3 1/2))))
+              (make-vtrack 2 3/2 2 '+)
+              (make-htrack 2 8/3 2 '+)
+              (make-vtrack 8/3 3/2 2 '+)
+              (make-driver 2 3/2 '(big small) #:color 'red)))
 
 
 
