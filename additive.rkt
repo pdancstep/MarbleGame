09da4d7f125b461a4dd849d2e3661b1777bfc41c
Jamie Perconti <jamieperconti@gmail.com>
2021-10-01 13:52:11 -0400

    switch level linear2 from quarters to thirds

--- additive.rkt
+++ additive.rkt
@@ -65,44 +65,37 @@
                     (make-driver .5 -2.5)
                     (make-follower -3 .5))))
 
-;similar paths, quarter scale
+;similar paths, third scale
 (define linear2
   (make-level (list (make-htrack -3 -1 .5)
-                    (make-vtrack -1 .5 2.5)
-                    (make-htrack -1 1 2.5)
-                    (make-vtrack 1 2.5 .5)
+                    (make-vtrack -1 .5 2)
+                    (make-htrack -1 1 2)
+                    (make-vtrack 1 2 .5)
                     (make-htrack 1 3 .5)
                     (make-goal 3 .5)
 
-                    (make-htrack -3.75 -3.25 -2.5 '+)
-                    (make-vtrack -3.25 -2.5 -2 '+)
-                    (make-htrack -3.25 -2.75 -2 '+)
-                    (make-vtrack -2.75 -2 -2.5 '+)
-                    (make-htrack -2.75 -2.25 -2.5 '+)
+                    (make-htrack -3 -7/3 -2 '+)
+                    (make-vtrack -7/3 -2 -3/2 '+)
+                    (make-htrack -7/3 -5/3 -3/2 '+)
+                    (make-vtrack -5/3 -3/2 -2 '+)
+                    (make-htrack -5/3 -1 -2 '+)
 
-                    (make-htrack -1.75 -1.25 -2.5 '+)
-                    (make-vtrack -1.25 -2.5 -2 '+)
-                    (make-htrack -1.25 -.75 -2 '+)
-                    (make-vtrack -.75 -2 -2.5 '+)
-                    (make-htrack -.75 -.25 -2.5 '+)
+                    (make-htrack -1 -1/3 -5/2 '+)
+                    (make-vtrack -1/3 -5/2 -2 '+)
+                    (make-htrack -1/3 1/3 -2 '+)
+                    (make-vtrack 1/3 -2 -5/2 '+)
+                    (make-htrack 1/3 1 -5/2 '+)
 
-                    (make-htrack 1.75 1.25 -2.5 '+)
-                    (make-vtrack 1.25 -2.5 -2 '+)
-                    (make-htrack 1.25 .75 -2 '+)
-                    (make-vtrack .75 -2 -2.5 '+)
-                    (make-htrack .75 .25 -2.5 '+)
+                    (make-htrack 1 5/3 -2 '+)
+                    (make-vtrack 5/3 -2 -3/2 '+)
+                    (make-htrack 5/3 7/3 -3/2 '+)
+                    (make-vtrack 7/3 -3/2 -2 '+)
+                    (make-htrack 7/3 3 -2 '+)
 
-                    (make-htrack 3.75 3.25 -2.5 '+)
-                    (make-vtrack 3.25 -2.5 -2 '+)
-                    (make-htrack 3.25 2.75 -2 '+)
-                    (make-vtrack 2.75 -2 -2.5 '+)
-                    (make-htrack 2.75 2.25 -2.5 '+)
-
-                    (make-driver -3.75 -2.5)
-                    (make-driver -1.75 -2.5)
-                    (make-driver .25 -2.5)
-                    (make-driver 2.25 -2.5)
-                    (make-follower -3 .5))))
+                    (make-driver -3 -2)
+                    (make-driver -1 -5/2)
+                    (make-driver 1 -2)
+                    (make-follower -3 1/2))))
 
 
 
